0a4b765b306566d1054205f0270f59736ab6fcde
Moslem Teyeb <teyebmoslem@gmail.com>
2025-04-13 10:33:43 +0100

    Add files via upload

---
+++ server/indexBackTesting.js
@@ -0,0 +1,19 @@
+const { createPerson, createFatherChildRelationship, createMarriage,
+  getFather, getGrandMother, getMother, getRelationship, getGender, 
+  createMotherChildRelationship, getHusbandID} = require('./neo4jRelationships');
+
+// Example function call to create a Father-Child relationship and fetch relationships
+const run = async () => {
+try {
+  // await createMarriage("Zohra")
+  await getHusbandID(13);
+
+
+
+} catch (error) {
+  console.error('Error executing functions:', error);
+}
+};
+
+// Run the function directly
+run();
