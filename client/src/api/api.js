2d0064a2740c91969f451d1498679035dad4128f
Moslem Teyeb <teyebmoslem@gmail.com>
2025-04-13 10:53:14 +0100

    Delete client directory

--- client/src/api/api.js
+++
@@ -1,6 +0,0 @@
-// client/src/api.js
-export async function fetchFamilyTree() {
-  const response = await fetch('http://localhost:5000/api/family-tree');
-  const data = await response.json();
-  return data;
-}
